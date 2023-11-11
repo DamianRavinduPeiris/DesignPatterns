2ea618772aad04026fae2d6a6bd3f2328cabd3b6
Damian Peiris. <drpeiris3@gmail.com>
2023-11-11 12:24:43 +0530

    Updated the Main method.

--- src/com/damian/dp/behaviroal/observable/Main.java
+++ src/com/damian/dp/behaviroal/observable/Main.java
@@ -3,11 +3,10 @@ package com.damian.dp.behaviroal.observable;
 public class Main {
     public static void main(String[] args) {
         Subject subject = new Subject();
+        subject.attach(new HexalObserver(subject));
+        subject.attach(new BinaryObserver(subject));
         subject.setState(11);
-        Observer o1 = new HexalObserver(subject);
-        Observer o2 = new BinaryObserver(subject);
-        o1.update();
-        o2.update();
+
 
 
     }
